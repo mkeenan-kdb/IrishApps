0b7e84c83ccfe76f0cfae0eabeb85b8d74ad3546
Michael Keenan <gaeilgeabubu@gmail.com>
2024-12-12 23:58:20 -0500

    header height offset for mobile

--- js/irish.js
+++ js/irish.js
@@ -1,4 +1,17 @@
+function scaleContent() {
+  var newWidth = document.body.offsetWidth;
+  var screenWidth = screen.width;
+  if(screenWidth == newWidth){
+    return;
+  }else{
+    pageWidth = newWidth;
+    document.querySelector("#appContainer").style.marginTop = document.querySelector(".web-title").offsetHeight + 20 + "px";
+  };
+}
+
 (function(){
   //document.getElementById('inputText').addEventListener('keyup', processText);
  console.log("page loaded!")
-})()
+ document.querySelector("#appContainer").style.marginTop = document.querySelector(".web-title").offsetHeight + 20 + "px";
+ window.onresize = scaleContent;
+})();
